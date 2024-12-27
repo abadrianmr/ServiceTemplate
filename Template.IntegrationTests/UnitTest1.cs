51faeec1742e94c72d21bde38185923076c09bec
4milrod <4milrod@gmail.com>
2024-12-27 14:21:43 +0100

    Add folder structure and projects

---
+++ Template.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Template.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ test/Template.ArchitectureTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Template.ArchitectureTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
